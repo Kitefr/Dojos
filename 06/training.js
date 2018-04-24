5c73346454623e7952f1822672365cc0891294b2
Kitefr <kitefr@gmail.com>
2018-04-24 10:08:52 +0200

    Preparation of repo for new TDD usage

--- 06/training.js
+++
@@ -1,9 +0,0 @@
-// https://regexr.com/
-// https://projects.lukehaas.me/regexhub/
-// Common regex https://gist.github.com/nerdsrescueme/1237767
-// Regex CheatSheet http://www.rexegg.com/regex-quickstart.html
-// Regex CheatSheet https://www.cheatography.com/davechild/cheat-sheets/regular-expressions/
-
-const chaine = "Je suis une chaine";
-const test = /^\w+$/.test(chaine);
-console.log(test);
